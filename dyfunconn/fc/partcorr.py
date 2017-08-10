Set the correct parameters to compute the covariance matrix to the partial correlation estimator.

dyfunconn/fc/partcorr.py
@@ -19,14 +19,17 @@ def partcorr(data, fb, fs, pairs=None):
 
     P_corr = np.zeros((n_channels, n_channels), dtype=np.float32)
 
-    r = np.cov(filtered)
+    r = np.cov(filtered, rowvar=False)
     r = np.float32(r)
 
     rinv = np.linalg.inv(r)
+    rinv = np.float32(rinv)
 
     for i in range(n_channels):
         for ii in range(n_channels):
             P_corr[i, ii] = -rinv[i, ii] / \
-                np.float32(np.sqrt(rinv[i, i] * rinv[ii, ii]))
+                (np.sqrt(rinv[i, i] * rinv[ii, ii]))
+
+    P_corr = np.float32(P_corr)
 
     return P_corr