* Fix PLV computation.
* Update relevant tests and examples.

dyfunconn/fc/plv.py
@@ -90,6 +90,7 @@ class PLV(Estimator):
 
         self.fb = fb
         self.fs = fs
+        self.data_type = np.complex
 
     def preprocess(self, data):
         _, _, u_phases = analytic_signal(data, self.fb, self.fs)
@@ -115,7 +116,7 @@ class PLV(Estimator):
         n_samples = len(ts1)
 
         ts_plv = np.exp(1j * (ts1 - ts2))
-        avg_plv = np.abs(np.sum((ts_plv)) / float(n_samples))
+        avg_plv = np.abs(np.sum((ts_plv))) / float(n_samples)
 
         return ts_plv, avg_plv
 
@@ -153,7 +154,7 @@ class PLV(Estimator):
         for pair in self.pairs:
             u_phases1, u_phases2 = data[pair, ]
             ts_plv = np.exp(1j * (u_phases1 - u_phases2))
-            avg_plv = np.abs(np.sum((ts_plv)) / float(n_samples))
+            avg_plv = np.abs(np.sum((ts_plv))) / float(n_samples)
 
             ts[pair] = ts_plv
             avg[pair] = avg_plv

examples/fc_plv.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-# np.set_printoptions(precision=3, linewidth=256)
+np.set_printoptions(precision=3, linewidth=256)
 
 from dyfunconn.fc import plv, PLV
 
 
 if __name__ == "__main__":
     data = np.load("/home/makism/Github/dyfunconn/examples/data/eeg_32chans_10secs.npy")
-    # data = data[0:5, ]
+    data = data[0:5, ]
 
-    # ts, avg = plv(data, [1.0, 4.0], 128.0)
+    ts, avg = plv(data, [1.0, 4.0], 128.0)
+    print avg
 
-    # print avg
-
-    p = PLV([1.0, 4.0], 128.0, pairs=None)
-    a = data[0, :]
-    b = data[1, :]
-
-    tmp, tmp2 = p.estimate_pair(a, b)
-    print tmp, tmp2
-
-    tmp, tmp2 = p.estimate(data[0:2, :])
-    print tmp, tmp2
+    # p = PLV([1.0, 4.0], 128.0, pairs=None)
+    # a = data[0, :]
+    # b = data[1, :]
+    #
+    # tmp, tmp2 = p.estimate_pair(a, b)
+    # print tmp, tmp2
+    #
+    # tmp, tmp2 = p.estimate(data[0:2, :])
+    # print tmp, tmp2