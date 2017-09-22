dc51e20dadfc3c9c51af1a185185886e261135f8
makis marimpis <makhsm@gmail.com>
2017-09-22 21:33:08 +0200

    Example of TVFCGs and correlation.

---
+++ examples/tvfcgs_corr.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import time
+
+import numpy as np
+np.set_printoptions(precision=3, linewidth=160, suppress=True)
+
+from dyfunconn.fc import Corr
+from dyfunconn import tvfcg
+
+
+if __name__ == '__main__':
+    data = np.load(
+        "/home/makism/Github/dyfunconn/examples/data/eeg_32chans_10secs.npy")
+
+    n_channels, n_samples = np.shape(data)
+
+    fb = [1.0, 4.0]
+    cc = 3.0
+    fs = 128
+    step = 5
+    estimator = Corr(fb, fs)
+
+    fcgs = tvfcg(data, estimator, fb, fs)
+
+    print(fcgs)
