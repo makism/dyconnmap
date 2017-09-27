3e375e9282739bf9c7263bb3bdebaafb0d0401a4
makis marimpis <makhsm@gmail.com>
2017-09-27 16:28:03 +0200

    Update code style.

--- dyfunconn/ts/dcorr.py
+++ dyfunconn/ts/dcorr.py
@@ -48,9 +48,9 @@ def dcorr(x, y):
     A = a - a.mean(axis=0)[None, :] - a.mean(axis=1)[:, None] + a.mean()
     B = b - b.mean(axis=0)[None, :] - b.mean(axis=1)[:, None] + b.mean()
 
-    dcov2_xy = (A * B).sum()/float(n * n)
-    dcov2_xx = (A * A).sum()/float(n * n)
-    dcov2_yy = (B * B).sum()/float(n * n)
-    dcor = np.sqrt(dcov2_xy)/np.sqrt(np.sqrt(dcov2_xx) * np.sqrt(dcov2_yy))
+    dcov2_xy = (A * B).sum() / float(n * n)
+    dcov2_xx = (A * A).sum() / float(n * n)
+    dcov2_yy = (B * B).sum() / float(n * n)
+    dcor = np.sqrt(dcov2_xy) / np.sqrt(np.sqrt(dcov2_xx) * np.sqrt(dcov2_yy))
 
     return dcor
