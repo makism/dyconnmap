3f8fc3a2b5087d0b268891bbad31227e1278fae5
makis marimpis <makhsm@gmail.com>
2017-12-16 14:35:09 +0100

    Example file using complexity index.

---
+++ examples/ts_ci.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+import numpy as np
+np.set_printoptions(precision=3, linewidth=256)
+
+from dyfunconn.ts import complexity_index
+
+
+if __name__ == "__main__":
+    rng = np.random.RandomState(0)
+
+    ts = rng.randint(1, 10, [100])
+    norm_ci, ci, spectrum = complexity_index(ts, sub_len=25, normalize=True)
+
+    print ("Normalized complexity index: %f" % (norm_ci))
+    print ("Complexity index: %f" % (ci))
