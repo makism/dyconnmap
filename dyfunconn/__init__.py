e4b3873a0028bf707dea8da68c865fec91222d75
makis marimpis <makhsm@gmail.com>
2018-03-18 17:52:10 +0100

    Remove sliding_window function.

--- dyfunconn/__init__.py
+++ dyfunconn/__init__.py
@@ -7,11 +7,9 @@
 __version__ = '0.99-git'
 
 from .tvfcgs import tvfcg, tvfcg_ts, tvfcg_cfc
-from .sliding_window import sliding_window
 from .analytic_signal import analytic_signal
 
 __all__ = ['analytic_signal',
-           'sliding_window',
            'bands',
            'tvfcg', 'tvfcg_cfc', 'tvfcg_ts',
            'sim_models']
