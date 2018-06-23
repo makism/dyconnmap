977282766ed9fdde034e48327b7a21422cf44a98
makis marimpis <makhsm@gmail.com>
2018-06-23 20:58:36 +0200

    Update unit tests.

--- dyfunconn/__init__.py
+++ dyfunconn/__init__.py
@@ -6,12 +6,12 @@
 
 __version__ = '0.99-git'
 
-from .tvfcgs import tvfcg, tvfcg_ts, tvfcg_cfc
+from .tvfcgs import tvfcg, tvfcg_ts, tvfcg_cfc, tvfcg_compute_windows
 from .sliding_window import sliding_window_indx
 from .analytic_signal import analytic_signal
 
 __all__ = ['analytic_signal',
            'bands',
-           'tvfcg', 'tvfcg_cfc', 'tvfcg_ts',
+           'tvfcg', 'tvfcg_cfc', 'tvfcg_ts', 'tvfcg_compute_windows'
            'sliding_window_indx',
            'sim_models']
