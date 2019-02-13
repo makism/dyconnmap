e74fdb4e3362734e4c92cebba157c36fdc7f0ed3
makis marimpis <makhsm@gmail.com>
2019-02-13 15:27:27 +0100

    Update nosetests to pass. Convert to float32/nan_to_num and use assert_all_close(x, y, rtol=1e-10, atol=0.0) where needed

--- tests/test_fc.py
+++ tests/test_fc.py
@@ -44,7 +44,7 @@ def test_coherence():
     coh = coherence(data, [1.0, 4.0], 128.0, **csdparams)
 
     expected = np.load("data/test_coherence.npy")
-    np.testing.assert_array_equal(coh, expected)
+    np.testing.assert_allclose(coh, expected, rtol=1e-10, atol=0.0)
 
 
 def test_dpli():
@@ -92,7 +92,7 @@ def test_icoherence():
     icoh = icoherence(data, [1.0, 4.0], 128.0, **csdparams)
 
     expected = np.load("data/test_icoherence.npy")
-    np.testing.assert_array_equal(icoh, expected)
+    np.testing.assert_allclose(icoh, expected, rtol=1e-10, atol=0.0)
 
 
 def test_iplv():
@@ -100,10 +100,10 @@ def test_iplv():
     ts, avg = iplv(data, [1.0, 4.0], 128.0)
 
     expected_ts = np.load("data/test_iplv_ts.npy")
-    np.testing.assert_array_equal(ts, expected_ts)
+    np.testing.assert_allclose(ts, expected_ts, rtol=1e-10, atol=0.0)
 
     expected_avg = np.load("data/test_iplv_avg.npy")
-    np.testing.assert_array_equal(avg, expected_avg)
+    np.testing.assert_allclose(avg, expected_avg, rtol=1e-10, atol=0.0)
 
 
 def test_mui():
@@ -144,16 +144,19 @@ def test_pac_multiple_channels():
 
     estimator = PLV(fb_lo, fs)
     ts, avg = pac(data, fb_lo, fb_hi, 128, estimator)
-
-    ts = np.real(ts)
+    ts = np.nan_to_num(ts)
+    avg = np.nan_to_num(avg)
 
     expected_ts = np.load("data/test_pac_plv_ts.npy")
-    expected_ts = np.real(ts)
-
+    expected_ts = np.nan_to_num(ts)
     np.testing.assert_array_equal(ts, expected_ts)
-
+    
+    avg = np.float32(avg)
+    
     expected_avg = np.load("data/test_pac_plv_avg.npy")
-    np.testing.assert_array_equal(avg, expected_avg)
+    expected_avg = np.nan_to_num(expected_avg)
+    expected_avg = np.float32(expected_avg)
+    np.testing.assert_allclose(avg, expected_avg, rtol=1e-10, atol=0.0)
 
 
 def test_pec():
@@ -179,10 +182,10 @@ def test_pli():
     ts, avg = pli(data, [1.0, 4.0], 128.0, pairs)
 
     expected_ts = np.load("data/test_pli_ts.npy")
-    np.testing.assert_array_equal(ts, expected_ts)
+    np.testing.assert_allclose(ts, expected_ts, rtol=1e-10, atol=0.0)
 
     expected_avg = np.load("data/test_pli_avg.npy")
-    np.testing.assert_array_equal(avg, expected_avg)
+    np.testing.assert_allclose(avg, expected_avg, rtol=1e-10, atol=0.0)
 
 
 def test_plv():
@@ -190,10 +193,10 @@ def test_plv():
     ts, avg = plv(data, [1.0, 4.0], 128.0)
 
     expected_ts = np.load("data/test_plv_ts.npy")
-    np.testing.assert_array_equal(ts, expected_ts)
+    np.testing.assert_allclose(ts, expected_ts, rtol=1e-10, atol=0.0)
 
     expected_avg = np.load("data/test_plv_avg.npy")
-    np.testing.assert_array_equal(avg, expected_avg)
+    np.testing.assert_allclose(avg, expected_avg, rtol=1e-10, atol=0.0)
 
 
 def test_rho_index():
@@ -216,9 +219,11 @@ def test_wpli():
     csdparams = {'NFFT': 256, 'noverlap': 256 / 2.0}
 
     wpliv = wpli(data, [1.0, 4.0], 128.0, **csdparams)
+    wpliv = np.nan_to_num(wpliv)
 
     expected = np.load("data/test_wpli.npy")
-    np.testing.assert_array_equal(wpliv, expected)
+    expected = np.nan_to_num(expected)
+    np.testing.assert_allclose(wpliv, expected, rtol=1e-10, atol=0.0)
 
 
 def test_dwpli():
@@ -230,7 +235,11 @@ def test_dwpli():
     dwpliv = dwpli(data, [1.0, 4.0], 128.0, **csdparams)
 
     expected = np.load("data/test_dwpli.npy")
-    np.testing.assert_array_equal(dwpliv, expected)
+    
+    dwpliv = np.nan_to_num(dwpliv)
+    expected = np.nan_to_num(expected)
+    
+    np.testing.assert_allclose(dwpliv, expected, rtol=1e-10, atol=0.0)
 
 
 def test_corr():
