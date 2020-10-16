121fe82893053ff1a8c5bd241dde931e93f3e979
Avraam Marimpis <makhsm@gmail.com>
2020-10-16 20:49:54 +0200

    Fix input and resulting matrices shapes.

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -58,7 +58,6 @@ def test_mutual_information():
 def test_nodal_global_efficiency():
     inv_mtx = np.load("data/test_graphs_inv_mtx.npy")
     result = np.load("data/test_graphs_nodal_global_efficiency.npy")
-    result = result.flatten()
 
     nodal_ge = nodal_global_efficiency(inv_mtx)
 
