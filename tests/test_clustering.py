Fix precision issues across platforms.

tests/test_clustering.py
@@ -117,4 +117,4 @@ def test_clustering_som_umatrix():
     umatrix = dyconnmap.cluster.umatrix(protos)
 
     result_umatrix = np.load("data/test_clustering_som_umatrix.npy")
-    np.testing.assert_array_equal(umatrix, result_umatrix)
+    np.testing.assert_array_almost_equal(umatrix, result_umatrix)

tests/test_fc.py
@@ -146,19 +146,10 @@ def test_iplv_nofilter():
     data = np.load("../examples/data/rois39_samples100.npy")
     ts, avg = iplv(data)
 
-    if "TRAVIS" in os.environ:
-        # We have to use the following to make the test work on Travis
-        expected_ts = np.load("data/test_iplv_nofilter_ts.npy")
-        np.testing.assert_allclose(ts, expected_ts, rtol=1e-10, atol=0.0)
-
-        expected_avg = np.load("data/test_iplv_nofilter_avg.npy")
-        np.testing.assert_allclose(avg, expected_avg, rtol=1e-10, atol=0.0)
-    else:
-        # The following tests pass locally; but they fail on Travis o_O
-        expected_ts = np.load("data/test_iplv_nofilter_ts.npy")
-        np.testing.assert_array_equal(ts, expected_ts)
-        expected_avg = np.load("data/test_iplv_nofilter_avg.npy")
-        np.testing.assert_array_equal(avg, expected_avg)
+    expected_ts = np.load("data/test_iplv_nofilter_ts.npy")
+    np.testing.assert_array_almost_equal(ts, expected_ts)
+    expected_avg = np.load("data/test_iplv_nofilter_avg.npy")
+    np.testing.assert_array_almost_equal(avg, expected_avg)
 
 
 def test_fast_iplv_nofilter():

tests/test_sliding_window.py
@@ -31,7 +31,7 @@ def test_sliding_window():
     if "TRAVIS" in os.environ:
         assert True
     else:
-        np.testing.assert_array_equal(dfcg_r, expected)
+        np.testing.assert_array_almost_equal(dfcg_r, expected)
 
 
 def test_sliding_window_indx():

tests/test_tvfcgs.py
@@ -96,9 +96,4 @@ def test_tvfcgs_pac_plv():
 def test_tvfcgs_from_plv_ts():
     result_fcgs = np.load("data/test_tvfcgs_from_plv_ts.npy")
 
-    if "TRAVIS" in os.environ:
-        # We have to use the following to make the test work on Travis
-        np.testing.assert_allclose(tvfcg_plv_ts, result_fcgs, rtol=1e-10, atol=0.0)
-    else:
-        # The following tests pass locally; but they fail on Travis o_O
-        np.testing.assert_array_equal(tvfcg_plv_ts, result_fcgs)
+    np.testing.assert_array_almost_equal(tvfcg_plv_ts, result_fcgs)