d4f39ab4bcf86620ce09e2057a65e3977b0e45ad
Avraam Marimpis <makhsm@gmail.com>
2020-11-15 11:35:13 +0100

    * Add helper method to setup the window given a dictionary of settings.
    * Add helper method to fetch the settings/configuration of a window object.

--- dyconnmap/future/dynamicwindow.py
+++ dyconnmap/future/dynamicwindow.py
@@ -1,3 +1,4 @@
+"""Base Dynamic Window."""
 from dataclasses import dataclass, field
 from abc import ABC, abstractmethod
 from typing import List, Type, Union, Optional, Tuple, Dict
@@ -7,10 +8,10 @@ from typing import List, Type, Union, Optional, Tuple, Dict
 class DynamicWindow(ABC):
     """Base dynamic window class."""
 
-    samples: int
-    rois: int
-    slides: int = field(init=False, default=0)
     window_length: int
+    samples: int = field(default=0)
+    rois: int = field(default=0)
+    slides: int = field(init=False, default=0)
     pairs: Optional[List[Tuple[int, int, int]]] = field(
         default=None,
         metadata={
@@ -18,9 +19,19 @@ class DynamicWindow(ABC):
         },
     )
 
-    @abstractmethod
-    def __post_init__(self):
-        ...
+    def prepare(self, **kwargs) -> None:
+        self.samples = kwargs.get("samples", 0)
+        self.rois = kwargs.get("rois", 0)
+
+    def settings(self) -> dict:
+        """Return the settings for the given estimator."""
+        return {
+            "window_length": self.window_length,
+            "samples": self.samples,
+            "rois": self.rois,
+            "slides": self.slides,
+            "pairs": self.pairs,
+        }
 
     @abstractmethod
     def __iter__(self):
