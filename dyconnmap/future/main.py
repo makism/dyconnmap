2fd5e80c4ae8aeff098f231999e7cf5c64dcdbb0
Avraam Marimpis <makhsm@gmail.com>
2020-11-15 11:54:33 +0100

    Update working example.

--- dyconnmap/future/main.py
+++ dyconnmap/future/main.py
@@ -9,16 +9,14 @@ from dyconnmap.fc import plv_fast
 from dataset import Dataset, Modality
 
 from correlation import Correlation
-from timevar import TimeVarying
+
+# from timevar import TimeVarying
 from slidingwindow import SlidingWindow
 from phaselockingvalue import PhaseLockingValue
 from pipeline import Pipeline
 from basicfilter import passband_filter
 
 if __name__ == "__main__":
-    # import pdb
-    # pdb.set_trace()
-
     rng = np.random.RandomState(0)
 
     n_subjects = 10
@@ -32,21 +30,25 @@ if __name__ == "__main__":
     # ds = Dataset(data, modality=Modality.FMRI, tr=1.5)
     ds = Dataset(data, modality=Modality.Raw, fs=128.0)
     ds.labels = ["a", "b"]
-    ds += data2
+    # ds += data2
     print(ds)
 
-    win = SlidingWindow(step=10, samples=128, rois=32, window_length=10)
-    win = TimeVarying(step=10, samples=128, rois=32, window_length=10)
+    win = SlidingWindow(step=5, window_length=10)
+    # win = TimeVarying(step=10, samples=128, rois=32, window_length=10)
     # print(win)
 
-    conn = Correlation()
-    # conn(ds, win)
-    print(conn)
+    conn = Correlation(rois=[0, 3])
+    # conn = PhaseLockingValue(
+    #     filter=passband_filter, filter_opts={"fs": 128.0, "fb": [1.0, 4.0]}
+    # )
 
-    conn = PhaseLockingValue(
-        filter=passband_filter, filter_opts={"fs": 128.0, "fb": [1.0, 4.0]}
-    )
+    result = conn(ds, win)
+    # result = conn(ds)
+
     print(conn)
+    print(win)
+
+    print(result)
 
     # pipeline = Pipeline(
     # stages=[
@@ -57,12 +59,13 @@ if __name__ == "__main__":
     # print(pipeline)
 
     # Check if our new class yields the same results as the previous `plv_fast`.
-    result = conn(ds)[0]
+    # result = conn(ds, win)["result"]  # [0]["result"]
+    # print(result)
 
-    f_data = passband_filter(data, fs=128.0, fb=[1.0, 4.0])
-    legacy_plv = np.asarray(plv_fast(f_data))
-
-    result = np.float32(result)
-    legacy_plv = np.float32(legacy_plv)
-
-    np.testing.assert_array_equal(legacy_plv, result)
+    # f_data = passband_filter(data, fs=128.0, fb=[1.0, 4.0])
+    # legacy_plv = np.asarray(plv_fast(f_data))
+    #
+    # result = np.float32(result)
+    # legacy_plv = np.float32(legacy_plv)
+    #
+    # np.testing.assert_array_equal(legacy_plv, result)
