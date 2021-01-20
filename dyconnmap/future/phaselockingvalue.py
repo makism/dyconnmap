ba1b5dbbb58830fd6ba27d9503d7af3f43dfe2b0
Avraam Marimpis <makhsm@gmail.com>
2021-01-20 10:02:51 +0100

    Intercept properly the size of the time series (if a window is defined).

--- dyconnmap/future/phaselockingvalue.py
+++ dyconnmap/future/phaselockingvalue.py
@@ -24,20 +24,35 @@ class PhaseLockingValue(Estimator):
     def estimate(self, data: np.ndarray, **kwargs) -> np.ndarray:
         rois = kwargs["rois"]
         samples = kwargs["samples"]
+        # print("samples: ", samples)
 
         # Cartesian product of all ROIs.
         tmp = list(range(rois))
         rois_pairs = list(itertools.product(*[tmp, tmp]))
+        # l = rois
+        # rois_pairs = [(roi1, roi2) for roi1 in range(0, l) for roi2 in range(0, l)]
+        # print(rois_pairs)
 
-        ts = np.zeros((rois, rois, samples), dtype=np.complex)
+        if "window" not in kwargs:
+            ts_size = samples
+        else:
+            ts_size = kwargs["window"]["window_length"]
+        # print("ts_size: ", ts_size)
+
+        # ts = np.zeros((rois, rois, ts_size), dtype=np.complex)
         avg = np.zeros((rois, rois))
 
-        for pair in rois_pairs:
-            u_phases1, u_phases2 = data[pair,]
-            ts_plv = np.exp(1j * (u_phases1 - u_phases2))
-            avg_plv = np.abs(np.sum((ts_plv))) / float(samples)
+        # print("np.shape(data): ", np.shape(data))
+        # print("np.shape(ts): ", np.shape(ts))
+        # print("np.shape(avg): ", np.shape(avg))
 
-            ts[pair] = ts_plv
+        for pair in rois_pairs:
+            # print(f"pair: {pair}")
+            u_phases1, u_phases2 = data[pair, :]
+            ts_plv = np.exp(1j * (u_phases1 - u_phases2))
+            avg_plv = np.abs(np.sum((ts_plv))) / float(ts_size)
+
+            # ts[pair] = ts_plv
             avg[pair] = avg_plv
 
         return avg
