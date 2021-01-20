b780be4f20a240aca1e5cf723d37bccd366ca1cb
Avraam Marimpis <makhsm@gmail.com>
2021-01-20 10:50:43 +0100

    Add two new tests to verify the new time-varying and sliding window implementations against the previous code.

--- dyconnmap/future/test_estimator.py
+++ dyconnmap/future/test_estimator.py
@@ -4,16 +4,19 @@ import sys
 
 sys.path.append("/home/makism/Github/dyconnmap-feature_dataset/")
 sys.path.append("/home/makism/Github/dyconnmap-feature_dataset/dyconnmap/future/")
-from dyconnmap.fc import plv_fast
+from dyconnmap import tvfcg, sliding_window
+from dyconnmap.fc import PLV, Corr, plv_fast
 
 from .dataset import Dataset, Modality
 from .basicfilter import passband_filter
 from .phaselockingvalue import PhaseLockingValue
 from .correlation import Correlation, correlation
+from .slidingwindow import SlidingWindow
+from .timevar import TimeVarying
 
 
 def test_phaselockingvalue():
-
+    """Test Phase Locking Value estimator."""
     rng = np.random.RandomState(0)
 
     n_subjects = 10
@@ -38,7 +41,38 @@ def test_phaselockingvalue():
     np.testing.assert_array_equal(legacy_plv, result)
 
 
+def test_phaselockingvalue_time_varying():
+    """Test Phase Locking Value estimator (Time-Varying)."""
+    rng = np.random.RandomState(0)
+
+    n_subjects = 1
+    n_rois = 4
+    n_samples = 128 * 3
+    fs = 128.0
+    cc = 2.0
+    step = 10
+    fb = [1.0, 4.0]
+
+    cc = 2.0
+    step = 5
+
+    data = rng.rand(n_subjects, n_rois, n_samples)
+    ds = Dataset(data, modality=Modality.Raw, fs=fs)
+
+    win = TimeVarying(step=step, cc=cc)
+    conn = PhaseLockingValue(
+        rois=None, filter=passband_filter, filter_opts={"fs": fs, "fb": fb}
+    )
+    result = conn(ds, win)
+
+    estimator = PLV(fb, fs)
+    fcgs = tvfcg(data[0, :, :], estimator, fb, fs, cc, step)
+
+    np.testing.assert_array_equal(result, fcgs)
+
+
 def test_correlation():
+    """Test Correlation estimator."""
     rng = np.random.RandomState(0)
 
     n_subjects = 1
@@ -57,3 +91,31 @@ def test_correlation():
     result2 = result2[0]
 
     np.testing.assert_array_equal(result1, result2)
+
+
+def test_correlation_sliding_window():
+    """Test Correlation estimator (Sliding Window)."""
+    rng = np.random.RandomState(0)
+
+    n_subjects = 1
+    n_rois = 32
+    n_samples = 128 * 3
+    fs = 128.0
+
+    window_length = 128
+    step = 10
+
+    data = rng.rand(n_subjects, n_rois, n_samples)
+    ds = Dataset(data, modality=Modality.Raw, fs=fs)
+
+    conn = Correlation(rois=None)
+    win = SlidingWindow(step=step, window_length=window_length)
+    result = conn(ds, win)
+    result = np.array(result)
+
+    estimator = Corr()
+    fcgs = sliding_window(
+        data[0, :, :], estimator, window_length=window_length, step=step
+    )
+
+    np.testing.assert_array_equal(result, fcgs)
