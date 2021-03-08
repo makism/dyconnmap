5c4aa05e025c737c68383a4dbb674cf7d970ee93
Avraam Marimpis <makhsm@gmail.com>
2021-03-08 21:05:39 +0100

    * Add function to parse VTC's (3rd version); probably we have to flip   around the axes.

--- dyconnmap/future/bv.py
+++ dyconnmap/future/bv.py
@@ -1,5 +1,139 @@
 """BrainVoyager helper functions."""
 
+import numpy as np
+import struct
+
+
+def bv_convert_coords(coords, from_ref_space) -> np.ndarray:
+    """Convert Coordinates from/to TAL/MNI/BV system."""
+    transf_coords = None
+
+    arr = np.array(coords)
+    if np.atleast_2d(arr):
+        if from_ref_space == "TAL" or from_ref_space == "MNI":
+            transf_coords = 128.0 - arr
+            transf_coords = transf_coords.astype(np.int32)
+            tmp = transf_coords[:, 0].copy()
+            transf_coords[:, 0], transf_coords[:, 1] = (
+                transf_coords[:, 1],
+                transf_coords[:, 2],
+            )
+            transf_coords[:, 2] = tmp
+        else:
+            transf_coords = arr
+
+    return transf_coords
+
+
+def bv_parse_vtc(fname):
+    """Parse a VTC file, supprt only the 3rd version of the format."""
+
+    metadata = {
+        "version": 3,
+        "fmr_filename": "",
+        "num_protos": 1,
+        "protos_filenames": list(),
+        "current_protocol": 0,
+        "data_type": 2,  # 1 -> short int, 2 -> float
+        "num_volumes": 0,
+        "vmr_resolution": 3,
+        "x_start": 57,
+        "x_end": 231,
+        "y_start": 52,
+        "y_end": 172,
+        "z_start": 59,
+        "z_end": 197,
+        "lr": None,  # 1 -> left-is-right, 2 -> left-is-left, 0 -> unknown
+        "ref_space_flag": None,  # 1 -> native, 2 -> ACPC, 3 -> Talarach, 0 -> Unknown
+        "tr": None,  # in MS
+    }
+
+    tc = None
+
+    with open(fname, "rb") as fp:
+        contents = fp.read()
+
+        # Version
+        buf = struct.unpack("h", contents[0:2])[0]
+        metadata["version"] = buf
+
+        if buf != 3:
+            return metadata
+
+        # FMR filename
+        fmr_filename = ""
+        idx = 2
+        while True:
+            buf = struct.unpack("c", contents[idx : idx + 1])[0]
+            char = buf.decode("utf-8")
+            if char == "\0":
+                idx += 1
+                break
+            metadata["fmr_filename"] += char
+            idx += 1
+
+        # Number of Protocols attached
+        buf = struct.unpack("h", contents[idx : idx + 2])[0]
+        metadata["num_protos"] = buf
+
+        # Protocol Filenames
+        idx += 2
+        for i in range(metadata["num_protos"]):
+            proto_fname = ""
+            while True:
+                buf = struct.unpack("c", contents[idx : idx + 1])[0]
+                char = buf.decode("utf-8")
+                if char == "\0":
+                    idx += 1
+                    break
+                proto_fname += char
+                idx += 1
+            metadata["protos_filenames"].append(proto_fname)
+
+        # Current Protocol, Data Type, Number of Volumes
+        fields = struct.unpack("3h", contents[idx : idx + 6])
+        idx += 6
+        metadata["current_protocol"], metadata["data_type"], metadata[
+            "num_volumes"
+        ] = fields
+
+        # X,Y,Z start and end
+        fields = struct.unpack("6h", contents[idx : idx + 12])
+        idx += 12
+        metadata["x_start"], metadata["x_end"], metadata["y_start"], metadata[
+            "y_end"
+        ], metadata["z_start"], metadata["z_end"] = fields
+
+        # LR convention, Reference Space flag, TR
+        fields = struct.unpack("ssf", contents[idx : idx + 8])
+        idx += 8
+        metadata["lr"] = fields[0]
+        metadata["ref_space_flag"] = fields[1]
+        metadata["tr"] = fields[2]
+
+        # Timecourses
+        DataType = metadata["data_type"]
+        XStart, XEnd = metadata["x_start"], metadata["x_end"]
+        YStart, YEnd = metadata["y_start"], metadata["y_end"]
+        ZStart, ZEnd = metadata["z_start"], metadata["z_end"]
+        VTCResolution = int(metadata["vmr_resolution"])
+        NumVolumes = metadata["num_volumes"]
+
+        DimX = int((XEnd - XStart) / VTCResolution)
+        DimY = int((YEnd - YStart) / VTCResolution)
+        DimZ = int((ZEnd - ZStart) / VTCResolution)
+
+        read_amount = int(DimX * DimY * DimZ * NumVolumes)
+        data_size = 4 if DataType == 2 else 2
+        data_code = "f" if DataType == 2 else "H"
+        tc = struct.unpack(
+            f"{read_amount}{data_code}", contents[idx : idx + read_amount * data_size]
+        )
+        tc = np.reshape(np.array(tc), [-1, 4])
+        print(tc.shape)
+
+    return metadata, tc
+
 
 def bv_parse_voi(fname):
     """Parse a VOI definition file."""
