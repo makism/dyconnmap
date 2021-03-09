d202e772c121c66c7c1bce9282554437497347f3
Avraam Marimpis <makhsm@gmail.com>
2021-03-09 20:39:10 +0100

    Pass all tests by default on Travis; the functions will be tested locally with proper BV data until I figure out the licensing option.

--- dyconnmap/future/test_bv.py
+++ dyconnmap/future/test_bv.py
@@ -1,18 +1,29 @@
 """Test suite for BrainVoyager help functions."""
 
+import os
+
 from .bv import bv_convert_coords, bv_parse_voi, bv_parse_vtc
 
 
 def test_bv_convert_coords():
     """Test convert coords (BV)."""
-    pass
+
+    # Disable test on Travis.
+    if "TRAVIS" in os.environ:
+        assert True
 
 
 def test_bv_parse_voi():
     """Test parsing VOI (BV)."""
-    pass
+
+    # Disable test on Travis.
+    if "TRAVIS" in os.environ:
+        assert True
 
 
 def test_bv_parse_vtc():
     """Test parsing VTC (BV)."""
-    pass
+
+    # Disable test on Travis.
+    if "TRAVIS" in os.environ:
+        assert True
