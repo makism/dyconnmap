341c8bd3f476aa7dfa00b558f8880385f4de6510
Avraam Marimpis <makhsm@gmail.com>
2021-03-19 16:54:15 +0100

    * Fix failing test due to flie renaming.
    * Update the tests for the submodule "tvfcgs" to comfor the latest restructuring.

--- tests/test_tvfcgs.py
+++ tests/test_tvfcgs.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+"""Tests for time-varying functional connectivity graphs module."""
+
 import pytest
 import scipy as sp
 from scipy import io
@@ -6,7 +8,6 @@ import numpy as np
 import os
 from numpy import testing
 
-# dynfunconn
 from dyconnmap import tvfcg, tvfcg_ts, tvfcg_cfc, tvfcg_compute_windows
 from dyconnmap.fc import PAC, PLV, plv
 
@@ -51,11 +52,17 @@ def initialize():
     return tvfcg_plv_ts, tvfcg_plv_fcgs, tvfcg_pac_plv_fcgs
 
 
-def test_tvfcgs_compute_windows(initialize):
-    tvfcg_plv_ts, tvfcg_plv_fcgs, tvfcg_pac_plv_fcgs = initialize
+def test_tvfcgs_compute_windows():
+    """Test the generated windows from the TVFCGs method."""
 
-    data = np.load("data/test_iplv_ts.npy")
+    # Groundtruth
+    result_windows = np.load("groundtruth/tvfcgs/compute_windows_windows.npy")
+    result_window_length = np.load("groundtruth/tvfcgs/compute_windows_window_length.npy")
 
+    # Data
+    data = np.load("groundtruth/fc/iplv_ts.npy")
+
+    # Run
     fb = [1.0, 4.0]
     fs = 128.0
     cc = 2.0
@@ -63,38 +70,51 @@ def test_tvfcgs_compute_windows(initialize):
 
     windows, window_length = tvfcg_compute_windows(data, fb, fs, cc, step)
 
-    result_windows = np.load("data/test_tvfcgs_compute_windows_windows.npy")
+    # Test
     np.testing.assert_array_equal(windows, result_windows)
-
-    result_window_length = np.load("data/test_tvfcgs_compute_windows_window_length.npy")
     np.testing.assert_array_equal(window_length, result_window_length)
 
 
 def test_tvfcgs_plv(initialize):
-    tvfcg_plv_ts, tvfcg_plv_fcgs, tvfcg_pac_plv_fcgs = initialize
+    """Test the TVFCGs with the PLV estimator."""
 
-    result_fcgs = np.load("data/test_tvfcgs_plv.npy")
+    # Groundtruth
+    result_fcgs = np.load("groundtruth/tvfcgs/plv.npy")
+
+    # Data (and Run)
+    tvfcg_plv_ts, tvfcg_plv_fcgs, tvfcg_pac_plv_fcgs = initialize
 
     f32_1 = np.float32(result_fcgs)
     f32_2 = np.float32(tvfcg_plv_fcgs)
 
+    # Test
     np.testing.assert_array_almost_equal(f32_1, f32_2)
 
 
 def test_tvfcgs_pac_plv(initialize):
-    tvfcg_plv_ts, tvfcg_plv_fcgs, tvfcg_pac_plv_fcgs = initialize
+    """Test the TVFCGs with the PAC/PLV estimator."""
 
-    result_ts = np.load("data/test_tvfcgs_pac_plv.npy")
+    # Groundtruth
+    result_ts = np.load("groundtruth/tvfcgs/pac_plv.npy")
+
+    # Data (and Run)
+    tvfcg_plv_ts, tvfcg_plv_fcgs, tvfcg_pac_plv_fcgs = initialize
 
     f32_1 = np.float32(result_ts)
     f32_2 = np.float32(tvfcg_pac_plv_fcgs)
 
+    # Test
     np.testing.assert_array_almost_equal(f32_1, f32_2)
 
 
 def test_tvfcgs_from_plv_ts(initialize):
+    """Test the TVFCGs as extracted from the PLV timeseries."""
+
+    # Groundtruth
+    result_fcgs = np.load("groundtruth/tvfcgs/from_plv_ts.npy")
+
+    # Data
     tvfcg_plv_ts, tvfcg_plv_fcgs, tvfcg_pac_plv_fcgs = initialize
 
-    result_fcgs = np.load("data/test_tvfcgs_from_plv_ts.npy")
-
+    # Test
     np.testing.assert_array_almost_equal(tvfcg_plv_ts, result_fcgs)
